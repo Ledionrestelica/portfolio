c3bf1e7f7d68aacee13a486b05bd261eed9e4d5c
ledionres <ledionrestelica7@gmail.com>
2024-08-01 03:32:02 +0200

    Padding error

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -75,7 +75,7 @@ const Projects = () => {
             whileInView={{ scale: 1, opacity: 1 }}
             transition={{ delay: 0.6, duration: 0.5 }}
             viewport={{ once: true }}
-            className="p-2 flex justify-center items-center aspect-video w-200px bg-button rounded-lg"
+            className="py-2 px-6 flex justify-center items-center aspect-video w-200px bg-button rounded-lg"
           >
             <div className="relative w-full h-full">
               <Image
